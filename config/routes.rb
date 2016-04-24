c0870b89f8c99bd503e485def80567e04c9ccd8d
Paul Downing <downing034@gmail.com>
2016-04-24 14:31:17 -0500

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root  'home#index'
+  get   '/about' => 'home#about'
 
-  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
