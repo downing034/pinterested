f75cee958ef023a6c3a46f48b1b43209ff44e8a2
Paul Downing <downing034@gmail.com>
2016-05-01 01:01:02 -0500

    remvoing aws code to see if issue resolves

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://punterest.herokuapp.com' }
 
   # config for paperclip and herokuap
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_region => 'us-east-1'
-    :s3_credentials => {
-      :bucket => ENV['punterested'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  # config.paperclip_defaults = {
+  #   :storage => :s3,
+  #   :s3_region => 'us-east-1'
+  #   :s3_credentials => {
+  #     :bucket => ENV['punterested'],
+  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
 end
