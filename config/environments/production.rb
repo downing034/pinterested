f78a1e95e2e670cf313335caf8253bfa77c37990
Paul Downing <downing034@gmail.com>
2016-04-30 23:57:39 -0500

    Fixing aws config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,8 @@ Rails.application.configure do
 
   # config for paperclip and herokuap
   config.paperclip_defaults = {
-    :storage => {
+    :storage => :s3,
+    :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
