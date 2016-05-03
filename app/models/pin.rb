cfa77487758001fd04a3eba060c1549b991d14cd
Paul Downing <downing034@gmail.com>
2016-05-03 00:17:21 -0500

    Added validation, category column, and some styling updates

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :pun_text, presence: { message: "Pun text must contain the text of the pun itself!" }
+  validates :category, presence: { message: "Be sure to choose the category your pun falls under! Example: Animals" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502030520) do
+ActiveRecord::Schema.define(version: 20160503043620) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "pun_text"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160502030520) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "category"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
